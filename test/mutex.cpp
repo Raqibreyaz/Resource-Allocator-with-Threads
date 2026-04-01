682331ae335d25a01dd862abab872def96964735
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-04-01 11:42:46 +0530

    feat: implement multi-consumer task queue with thread-safe synchronization in mutex test

--- test/mutex.cpp
+++ test/mutex.cpp
@@ -1,42 +1,72 @@
+#include <chrono>
 #include <condition_variable>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <mutex>
+#include <queue>
 #include <thread>
 
+/*
+1. will use queue for multiple consumers
+2. will use semaphore to see what better we can do!
+- will use loop so that producer and consumer will do their work in repeat
+*/
 
 /*
-1. will use loop so that producer and consumer will do their work in repeat
-2. will use semaphore to see what better we can do!
+
+1. queue can have multiple tasks
+2.
+
 */
 
 int main() {
 
   std::mutex mtx;
   std::condition_variable cond;
-  bool isProduced = false;
+  std::queue<std::function<void()>> tasks;
+  bool isStopped = false;
 
   const auto producer = [&]() {
-    {
+    for (int i = 1; i <= 5; i++) {
       std::unique_lock<std::mutex> lock(mtx);
+      tasks.push([] { std::clog << "Executing Produced Data!" << std::endl; });
 
-      isProduced = true;
-      std::clog << "Data Produced!" << std::endl;
+      std::clog << "Pushed Task!" << std::endl;
+      lock.unlock();
+      cond.notify_one();
+
+      std::this_thread::sleep_for(std::chrono::seconds(2));
     }
-    cond.notify_one();
+
+    // cond.notify_all();
   };
 
   const auto consumer = [&]() {
-    std::unique_lock<std::mutex> lock(mtx);
-    cond.wait(lock, [&] { return isProduced; });
+    while (true) {
+      std::unique_lock<std::mutex> lock(mtx);
+      cond.wait(lock, [&] { return !tasks.empty(); });
 
-    std::clog << "Data Consumed!" << std::endl;
-    isProduced = false;
+      const auto task = tasks.front();
+      tasks.pop();
+
+      lock.unlock();
+      cond.notify_one();
+
+      if (task) {
+        task();
+        std::clog << "Data Consumed By " << std::this_thread::get_id()
+                  << std::endl;
+      }
+    }
   };
 
-  std::thread c(consumer), p(producer);
+  std::thread c1(consumer);
+  std::thread c2(consumer);
+  std::thread p(producer);
 
-  c.join();
+  c1.join();
+  c2.join();
   p.join();
 
   return EXIT_SUCCESS;
